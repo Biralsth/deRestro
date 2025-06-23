0fe9fc0f4484e2df6b32980f2097fa45c9a4a37c
Biralsth <theonepieceisreal333@gmail.com>
2025-06-23 17:43:02 +0545

    states

--- src/component/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Button({ lable, color }) {
-    return (
-        <div className='p-5 m-5 rounded' >
-            <button className={` text-white px-3 py-1 rounded ${color}`}>{lable}</button>
-        </div>
-    )
-}
